b57330e154b9bf5365ee600627fa26a9c1b777fe
Richard Jansen <richard@1337h4xbook>
2016-03-21 09:49:49 -0400

    yay

--- ass1.c
+++ ass1.c
@@ -4,14 +4,27 @@
 
 char main(){
    char answer[100];
+   char group[100];
    int status;
+   char command[100];
+
+   printf("Enter your groupnumber: ");
+   scanf("%s",group);
+   printf("\n");
+
    printf("Enter answer to assignment 1 :");
    scanf("%s",answer);
    printf("\n");
+
+   strcpy(command, "python test.py ");
+   strcat(command, group);
+   strcat(command, " ");
+   strcat(command, answer);
+
    
    if (strcmp(answer, "juist") == 0){
 	   printf("it passed\n");
-	   status = system("python test.py true false");
+	   status = system(command);
 	   exit(0);
    }
 
