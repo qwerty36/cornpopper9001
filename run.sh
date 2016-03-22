899af3a9f765cca7ec0cf94583536022ba1e9a8c
Richard Jansen <richard@1337h4x.nl>
2016-03-22 09:21:05 -0400

    More efficient AND music overlap fixed

--- run.sh
+++ run.sh
@@ -4,7 +4,18 @@ clear
 echo "$(tput setaf 2)"
 echo "starting script..."
 sleep 1
-mpg123 song1.mp3 &
+
+
+if pgrep "mpg123" > /dev/null
+then
+    echo 
+else
+    mpg123 -Z song1.mp3 &
+    echo
+    echo "Music started!"
+fi
+
+
 echo
 echo
 echo
@@ -30,61 +41,17 @@ if [[ ! $REPLY =~ ^(1|2|3|4|5)$ ]]; then
 	sleep 1
 fi
 
-#script for assigment 1
-if [[ $REPLY =~ ^[1]$ ]]
+#script for assigment
+if [[ $REPLY =~ ^(1|2|3|4|5)$ ]]
 then
 echo "$(tput setaf 1)"
 clear
-echo "You chose script 1"
+echo "You chose script :"${REPLY}
 echo "$(tput setaf 2)"
-./ass1
+./ass${REPLY}
 
 fi
 
-
-#script for assigment 2
-if [[ $REPLY =~ ^[2]$ ]]
-then
-echo "$(tput setaf 1)"
-clear
-echo "You chose script 2"
-echo "$(tput setaf 2)"
-./ass2
-
-fi
-
-#script for assigment 3
-if [[ $REPLY =~ ^[3]$ ]]
-then
-echo "$(tput setaf 1)"
-clear
-echo "You chose script 3"
-echo "$(tput setaf 2)"
-./ass3
-
-fi
-
-#script for assigment 4
-if [[ $REPLY =~ ^[4]$ ]]
-then
-echo "$(tput setaf 1)"
-clear
-echo "You chose script 4"
-echo "$(tput setaf 2)"
-./ass4
-
-fi
-
-#script for assigment 5
-if [[ $REPLY =~ ^[5]$ ]]
-then
-echo "$(tput setaf 1)"
-clear
-echo "You chose script 5"
-echo "$(tput setaf 2)"
-./ass5
-
-fi
 
 sleep 2.5
-exec ./run2.sh
+exec ./run.sh
